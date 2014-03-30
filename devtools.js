3caa302cc12607a5bfb52039873cfd1ad2142a43
Razvan Caliman <razvan.caliman@gmail.com>
2014-03-30 18:41:09 +0100

    sidebar first pass; trigger edit on selecte element

--- devtools.js
+++ devtools.js
@@ -1,81 +1,105 @@
 // var port = null,
-//     editorToggle = null,
-//     transformToggle = null,
-//     shapeType = null,
-//     isEditorOn = false
 
-// chrome.devtools.panels.elements.createSidebarPane(
-//     "CSS Shapes",
-//     function(sidebar) {
-//       sidebar.setPage('index.html')
-//       sidebar.setHeight('200px')
-//       // sidebar.onShown.addListener(setup)
-//       
-//       // port = chrome.runtime.connect({name: "devtools"});
-//       // port.onMessage.addListener(function(data) {
-//       //   // TODO treat incoming message
-//       // });
-//        
-// });
+var doc = null;
+var handlers = {
+  "action-edit": function(target){
+    var parent   = target.parentNode,
+        property = parent.querySelector('.property').textContent,
+        value    = parent.querySelector('.value').textContent;
 
-chrome.devtools.panels.elements.onSelectionChanged.addListener(function(){
-  // if (!isEditorOn){
-  //   return
-  // }
-  
-  // chrome.devtools.inspectedWindow.eval("(" + markElement.toString() + ")()", function(response){
-  //   // TODO use reponse.key and send to editor
-  //   turnOnEditor()
-  // })
+    if (property && value){
+      setupEditor(property, value);
+    }
+  }
+}
 
-  console.log('onelement selected')
-  chrome.devtools.inspectedWindow.eval("setSelectedElement($0)",
-      { useContentScriptContext: true });
+function onSelectElement(cb){
+    chrome.devtools.panels.elements.onSelectionChanged.addListener(cb);
+}
+
+chrome.devtools.panels.elements.createSidebarPane(
+    "CSS Shapes",
+    function(sidebar) {
+      sidebar.setPage('sidebar.html');
+      sidebar.setHeight('200px');
+      sidebar.onShown.addListener(setupSidebar);
+      // TODO: teardown on onHide
+
+      // port = chrome.runtime.connect({name: "devtools"});
+      // port.onMessage.addListener(function(data) {
+      //   // TODO treat incoming message
+      // });
 });
 
-console.log('sidebar ready')
+/*
+  Setup sidebar content and handlers after it has been added to the panel.
+
+  @param {Window} win The 'window' object of the sidebar page.
+  @return {undefined}
+*/
+function setupSidebar(win) {
+
+  // expose sidebar's document to global extension scope
+  doc = win.document;
+
+  doc.addEventListener("click", function(e){
+    var handler = handlers[e.target.className];
+    if (handler && typeof handler === 'function'){
+      handler.call(this, e.target);
+    }
+  })
+
+  // drop the editor then switching to a new element;
+  onSelectElement(teardownEditor);
+
+  onSelectElement(populateSidebar);
+  // collect data from current selected element on first load.
+  populateSidebar.call(this);
+}
+
+/*
+  Generate the contents of the sidebar using the computed style
+  of the currently selected element from the Elements panel.
+
+  @return {undefined}
+*/
+function populateSidebar(){
+
+  function handler(styleString){
+    // const SUPPORTED_PROPERTIES = ["shape-outside","shape-inside","clip-path"];
+
+    var style = JSON.parse(styleString);
+    var props = doc.querySelectorAll('.properties > li');
+
+    Array.prototype.forEach.call(props, function(prop) {
+      var element = prop.querySelector('.value');
+      var value = style[prop.id];
+
+      if (value) {
+        element.textContent = value;
+      } else {
+        console.warn(value);
+        prop.setAttribute('disabled','true');
+      }
+    });
+  }
+
+  // TODO: figure out why regular getComputedStyle returns object with keys, but no values.
+  chrome.devtools.inspectedWindow.eval("JSON.stringify(window.getComputedStyle($0, null))", handler);
+}
+
+function teardownEditor(){
+  chrome.devtools.inspectedWindow.eval('teardown()', { useContentScriptContext: true });
+}
+
+function setupEditor(property, value){
+  chrome.devtools.inspectedWindow.eval('setup($0, "'+ property.toString() +'", "'+ value.toString() +'")',
+      { useContentScriptContext: true });
+}
+
+// chrome.devtools.inspectedWindow.eval("(" + markElement.toString() + ")()", function(response){
 
 // var turnOnEditor = function(){
 //   port.postMessage({command: 'editor:on', shapeType: shapeType })
 //   transformToggle.checked = false
-// }
-// 
-// var turnOffEditor = function(){
-//   port.postMessage({command: 'editor:off', shapeType: shapeType })
-//   transformToggle.checked = false
-// }
-// 
-// var toggleTransformEditor = function(){
-//   port.postMessage({command: 'editor:transform', shapeType: shapeType })
-// }
-// 
-// // win is the 'window' object of the page inserted with sidebar.setPage()
-// var setup = function(win){
-//   if (!editorToggle){
-//     
-//     var shapeToggles = win.document.querySelectorAll('[name="shape-type"]')
-//     Array.prototype.forEach.call(shapeToggles, function(radio){
-//       
-//       if (radio.checked){
-//         shapeType = radio.value
-//       }
-//       
-//       radio.addEventListener('change', function(e){
-//         if (e.target.checked){
-//           shapeType = e.target.value
-//         }
-//         
-//         onElementSelected()
-//       })
-//     })
-//     
-//     editorToggle = win.document.querySelector('[data-editor-toggle]')
-//     editorToggle.addEventListener('change', function(e){
-//       isEditorOn = e.target.checked
-//       isEditorOn ? onElementSelected() : turnOffEditor()
-//     })
-//     
-//     transformToggle = win.document.querySelector('[data-transform-toggle]')
-//     transformToggle.addEventListener('change', toggleTransformEditor)
-//   }
 // }
--- driver.js
+++ driver.js
@@ -13,7 +13,7 @@
 //     editor && editor.toggleTransformEditor()
 //   }
 // }
-// 
+//
 // function getConfigForShape(target, shape){
 //   var prefix = '-webkit-'
 //   var defaultConfig = {
@@ -21,62 +21,62 @@
 //       path: { stroke: 'blue' },
 //       point: { fill: 'blue'}
 //     },
-// 
+//
 //     'shape-outside': {
 //       path: { stroke: 'red' },
 //       point: { fill: 'red'}
 //     },
-// 
+//
 //     'clip-path': {
 //       path: { stroke: 'lime' },
 //       point: { fill: 'lime'}
 //     }
 //   }
-//   
+//
 //   if (!target || !(shape in defaultConfig)){
 //     return
 //   }
-//   
+//
 //   // attempt to get the shape value for the shape property
 //   defaultConfig[shape].shape = window.getComputedStyle(target, null)[prefix + shape]
-//   
+//
 //   return defaultConfig[shape]
 // }
-// 
+//
 // function getShapeChangeHandler(property){
 //   return function(){
 //     var prefix = '-webkit-'
 //     editor.element.style[prefix + property] = editor.getPolygonPath()
 //   }
 // }
-// 
+//
 // function removeEditor(){
 //   if (!editor)
 //     return
-//     
+//
 //   editor.destroy()
 //   editor = null
 // }
-// 
+//
 // function setupEditor(){
 //   // get the selected element; marked by the devtools page
 //   var target = document.querySelector('[data-marker]')
-//   
+//
 //   // don't setup editor on its placeholder
 //   if (!target || target.getAttribute('data-role') === 'shape-editor'){
 //     return
 //   }
-//   
+//
 //   // Yo, Raphael, wake up!
 //   Raphael.eve("raphael.DOMload")
-//   
+//
 //   var config = getConfigForShape(target, shapeType)
 //   var handler = getShapeChangeHandler(shapeType)
-//   
+//
 //   editor = new ShapeOverlay(target, config)
 //   editor.shapechange(handler)
 // }
-// 
+//
 // port.onMessage.addListener(function(data, sender) {
 //   // look for a valid command name
 //   if (messages[data.command]){
@@ -84,7 +84,7 @@
 //     messages[data.command].call()
 //   }
 // });
-// 
+//
 // // pressing the 't' letter key will trigger the transforms editor
 // document.addEventListener('keypress', function(e){
 //   if(editor && e.keyCode === 116){
@@ -92,42 +92,36 @@
 //   }
 // })
 
+var editor, property, value;
 
-var Controller = (function(){
-    
-    var _providers = {},
-        _activeEditor, 
-        _property,
-        _hasTransformEditor,
-        
-        _config = {
-            'shape-inside': {
-              path: { stroke: 'blue' },
-              point: { fill: 'blue'}
-            },
-            
-            'shape-outside': {
-              path: { stroke: 'red' },
-              point: { fill: 'red'}
-            },
-            
-            'clip-path': {
-              path: { stroke: 'lime' },
-              point: { fill: 'lime'}
-            }
-        }
-    
-    return {
-        setup: function(el, model){
-            
-        },
-        
-        remove: function(){
-            
-        }
+function setup(el, property, value){
+    console.warn('I received a selected element!', el, property, value)
+    teardown();
+
+    // property = 'shape-outside';
+    // value = getComputedStyle(el, null)[property];
+    editor = new CSSShapesEditor(el, value)
+
+    editor.on('shapechange', function(){
+      console.log(editor.target.style[property] = this.getCSSValue())
+    })
+}
+
+function teardown(){
+    if (!editor){
+      return;
     }
-})()
 
-function setSelectedElement(el){
-    console.warn('I received a seleted element!', el)
+    property = undefined;
+    value = undefined;
+    editor.remove();
+    editor = null;
+}
+
+function toggleTransform(){
+    if (!editor){
+      return;
+    }
+
+    editor.toggleTransformEditor();
 }
