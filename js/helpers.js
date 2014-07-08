fd1473e86ad21f3e05a9a06efa7238b660e3e1da
Razvan Caliman <razvan.caliman@gmail.com>
2014-07-08 17:29:10 +0100

    give credit to todomvc project

--- js/helpers.js
+++ js/helpers.js
@@ -1,16 +1,6 @@
-// Copyright (c) 2014 Adobe Systems Incorporated. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// COPYRIGHT todomovc.com project: Addy Osmani, Sindre Sorhus, Pascal Hartig, Stephen Sawchuk
+// This file is mostly based on:
+// https://github.com/tastejs/todomvc/blob/gh-pages/vanilla-examples/vanillajs/js/helpers.js
 
 /*global NodeList */
 (function (window) {
