6f380b9b1223bf4ea3eb526f1aa9f76c022f6437
Razvan Caliman <razvan.caliman@gmail.com>
2014-07-16 11:32:09 +0100

    listen to DOM mutations

--- contentscript.js
+++ contentscript.js
@@ -14,11 +14,33 @@
 
 var port, timeout,
     delay = 100,
-    editors = {};
+    editors = {},
+    observers = {},
+    ignoreMutation = false; // 'true' while editor mutates the element and is expected
 
 function setup(el, property, value){
-    var options = {},
-        editor;
+    function _onShapeChange(){
+      var value = editor.getCSSValue();
+
+      ignoreMutation = true;
+
+      // sync element style;
+      editor.target.style[property] = value;
+
+      ignoreMutation = false;
+
+      // sync extension; throttle messages
+      if (!timeout){
+        timeout = window.setTimeout(function(){
+          signalUpdate(property, value);
+          window.clearTimeout(timeout);
+          timeout = undefined;
+        }, delay);
+      }
+    }
+
+    var editor,
+        options = {};
 
     switch (property) {
       case "shape-inside":
@@ -34,33 +56,14 @@ function setup(el, property, value){
     }
 
     editor = new CSSShapesEditor(el, value, options);
-    port = port || chrome.runtime.connect({name: "page"});
 
-    function onShapeChange(){
-
-      var message = {
-        type: 'update',
-        property: property,
-        value: editor.getCSSValue()
-      };
-
-      editor.target.style[property] = message.value;
-
-      // throttle messages to extension
-      if (!timeout){
-        timeout = window.setTimeout(function(){
-          port.postMessage(message);
-          window.clearTimeout(timeout);
-          timeout = undefined;
-        }, delay);
-      }
-    }
-
-    editor.on('shapechange', onShapeChange);
-
-    editor.on('ready', onShapeChange)
-
+    // TODO: figure out why PolygonEditor does not trigger 'shapechange' on first run, then remove on('ready')
+    editor.on('shapechange', _onShapeChange);
+    editor.on('ready', _onShapeChange)
     editors[property] = editor;
+
+    // observe the element for style changes
+    observe(el, property);
 }
 
 function remove(property){
@@ -68,10 +71,75 @@ function remove(property){
       return;
     }
 
+    if (observers[property]){
+      observers[property].disconnect();
+      delete observers[property];
+    }
+
     editors[property].off('shapechange');
     editors[property].off('ready');
     editors[property].remove();
     delete editors[property];
+}
+
+/*
+  Observe changes to the element's given style property.
+  If the new value is supported sync to the shapes editor.
+  If the new value is not supported, remove everything;
+  @param {HTMLElement} el
+  @param {String} property
+*/
+function observe(el, property){
+  var observer;
+
+  function _onMutation(){
+
+    // if 'true', mutations are coming from the editor; do not duplicate
+    if (ignoreMutation){
+      console.info('safe to ignore')
+      return;
+    }
+
+    var value = window.getComputedStyle(el)[property];
+
+    // naïve check to guard against values like 'none', 'inherit' or 'border-box'
+    if (value && value !== 'none' && value.indexOf('(') > -1){
+      // updating the editor will trigger 'shapechange', which will update extension
+      editors[property].update(value);
+    } else {
+      // TODO: treat non-function values smartly.
+      signalUpdate(property, 'none');
+      signalRemove(property);
+      remove(property);
+    }
+  }
+
+  observer = new MutationObserver(_onMutation);
+  observer.observe(el, {
+    childList: false,
+    characterData: false,
+    attributes: true,
+    attributeFilter: ['style']
+  });
+
+  observers[property] = observer;
+}
+
+function signalRemove(property){
+  port = port || chrome.runtime.connect({name: "page"});
+  port.postMessage({
+    type: 'remove',
+    property: property
+  });
+}
+
+function signalUpdate(property, value){
+  port = port || chrome.runtime.connect({name: "page"});
+  port.postMessage({
+    type: 'update',
+    property: property,
+    value: value
+  });
 }
 
 document.addEventListener('keyup', function(e){
@@ -81,12 +149,7 @@ document.addEventListener('keyup', function(e){
 
     case ESC_KEY:
       Object.keys(editors).forEach(function(property){
-        var message = {
-          type: 'remove',
-          property: property
-        };
-
-        port.postMessage(message);
+        signalRemove(property);
         remove(property);
       });
     break;
